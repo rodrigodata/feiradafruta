3022e6c315f5740414be7b9986120ea42321fced
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-04-14 01:27:34 -0300

    WIP - Estrutura Financeiro

--- pedido/app/constants/App.js
+++ pedido/app/constants/App.js
@@ -2,8 +2,12 @@ const DB_HOST = process.env.DB_HOST
   ? process.env.DB_HOST
   : "mongodb://localhost:27017/feiradafruta";
 const SQS_URL = process.env.SQS_URL;
+const STATUS_PEDIDO = {
+  PROGRAMADO: 1
+};
 
 module.exports = {
   DB_HOST,
-  SQS_URL
+  SQS_URL,
+  STATUS_PEDIDO
 };
